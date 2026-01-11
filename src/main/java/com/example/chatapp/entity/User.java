345d15c51d8f94155e4f11c8c2bfaf128df5a582
Harisankar <harisankar2206@gmail.com>
2026-01-11 19:41:23 +0530

    Create User.java

---
+++ src/main/java/com/example/chatapp/entity/User.java
@@ -0,0 +1,18 @@
+package com.example.chatapp.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String language;
+}
