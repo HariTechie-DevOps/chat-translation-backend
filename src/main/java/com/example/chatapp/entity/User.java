9033744cc3dd208be0b1627b764a11f22e742a49
Harisankar <harisankar2206@gmail.com>
2026-01-11 20:38:23 +0530

    Update User.java

--- src/main/java/com/example/chatapp/entity/User.java
+++ src/main/java/com/example/chatapp/entity/User.java
@@ -3,6 +3,7 @@ package com.example.chatapp.entity; // Must match folder
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
