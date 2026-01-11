0d601ed7bd7b0bb5e2493f036d2432c2b55b867c
Harisankar <harisankar2206@gmail.com>
2026-01-11 21:22:23 +0530

    Update ChatRoomRepository.java

--- src/main/java/com/example/chatapp/repository/ChatRoomRepository.java
+++ src/main/java/com/example/chatapp/repository/ChatRoomRepository.java
@@ -2,9 +2,12 @@ package com.example.chatapp.repository;
 
 import com.example.chatapp.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-    Optional<ChatRoom> findByUser1IdAndUser2IdOrUser1IdAndUser2Id(Long u1, Long u2, Long u2Again, Long u1Again);
+    
+    @Query("SELECT c FROM ChatRoom c WHERE (c.user1Id = :u1 AND c.user2Id = :u2) OR (c.user1Id = :u2 AND c.user2Id = :u1)")
+    Optional<ChatRoom> findChatRoom(@Param("u1") Long u1, @Param("u2") Long u2);
 }
